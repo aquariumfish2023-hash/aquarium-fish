0fbfa1836197814dd5e8b11f76e82ec9fc806a00
aquariumfish2023-hash <aquariumfish2023@gmail.com>
2026-09-06 16:18:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-const CACHE='aquarium-fish-v7';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg','./splash-v7.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='aquarium-fish-v8';
+const ASSETS=[
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.svg',
+  './splash-v7.png'
+];
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      )
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>
+      caches.match(e.request)
+    )
+  );
+});
