69420a913e359231605eac8d33199808cef08d5a
aquariumfish2023-hash <aquariumfish2023@gmail.com>
2026-09-23 17:43:32 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,39 @@
-const CACHE='aquarium-fish-v23';
-const ASSETS=[
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon.svg',
-  './splash-v7.png',
-  './aquarium-portada-wide.png',
-  './aquarium-portada-mobile.png'
+const CACHE = "catalogo-peces-v2-15-categorias";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./logo-empresa.jpg"
 ];
-
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
   );
 });
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-      )
-    ).then(()=>self.clients.claim())
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request).catch(()=>
-      caches.match(e.request)
-    )
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
